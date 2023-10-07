1473464f38bdeade30a222abedd360f16470bb63
giorgio <giorgio.programmer@gmail.com>
2023-10-07 09:30:39 +0200

    dadas

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Manrope } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], display: 'swap', variable: "--font-in" })
+
+const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: "--font-mr" })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${inter.manrope}`}>{children}</body>
     </html>
   )
 }
