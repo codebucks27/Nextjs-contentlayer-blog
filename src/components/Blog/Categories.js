46d04bdc97b8759af194e7f86cce363c44578245
Jamalissaka <88450988+Jamalissaka@users.noreply.github.com>
2023-12-15 18:48:13 +0000

    Update Categories.js

--- src/components/Blog/Categories.js
+++ src/components/Blog/Categories.js
@@ -1,22 +1,20 @@
-import { cx } from "@/src/utils";
-import Link from "next/link";
+import { slug } from "github-slugger";
 import React from "react";
+import Category from "./Category";
 
-const Category = ({ link = "#", name, active, ...props }) => {
+const Categories = ({ categories, currentSlug }) => {
   return (
-    <Link
-      href={link}
-      className={cx(
-        "inline-block py-1.5  md:py-2 px-6  md:px-10   rounded-full border-2 border-solid border-dark dark:border-light hover:scale-105 transition-all ease duration-200 m-2",
-        props.className,
-        active
-          ? "bg-dark text-light dark:bg-light dark:text-dark"
-          : "bg-light text-dark dark:bg-dark dark:text-light"
-      )}
-    >
-      #{name}
-    </Link>
+    <div className=" px-0 md:px-10 sxl:px-20 mt-10 border-t-2 text-dark dark:text-light border-b-2 border-solid border-dark dark:border-light py-4 flex items-start flex-wrap font-medium mx-5 md:mx-10">
+      {categories.map((cat) => (
+        <Category
+          key={cat}
+          link={`/categories/${cat}`}
+          name={cat}
+          active={currentSlug === slug(cat)}
+        />
+      ))}
+    </div>
   );
 };
 
-export default Category;
+export default Categories;
